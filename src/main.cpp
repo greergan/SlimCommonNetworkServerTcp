27c9b002c70410fa5cc09a05209a92fc7b5ffad8
Jeff Greer <geergan@gmail.com>
2026-08-03 21:22:19 -0700

    config: add conditional logging for SlimCommonNetworkServerTcp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <cstring>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 #include <slim/common/io/operations.h>
@@ -9,71 +10,156 @@
 #include <slim/common/io/runtime.h>
 #include <slim/common/io/task.h>
 #include <slim/common/network/server/tcp.h>
+#ifdef ENABLE_LOGGING
 #include <slim/common/log.h>
+#endif
 
 namespace slim::common::network::server {
 using namespace slim::common;
 namespace {
 
 ErrorStatus make_listen_fd(const tcp::Config& config, int& out_fd) noexcept {
+#ifdef ENABLE_LOGGING
+    log::trace({__func__, "begins", __FILE__, __LINE__});
+#endif
     int fd = ::socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, "socket() failed", __FILE__, __LINE__});
+        log::trace({__func__, "ends (ListenSocketCreationFailed)", __FILE__, __LINE__});
+#endif
         return ErrorStatus::ListenSocketCreationFailed;
     }
+#ifdef ENABLE_LOGGING
+    log::debug({__func__, std::format("socket created fd={}", fd), __FILE__, __LINE__});
+#endif
     int opt = 1;
     ::setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+#ifdef ENABLE_LOGGING
+    log::debug({__func__, std::format("SO_REUSEADDR set fd={}", fd), __FILE__, __LINE__});
+#endif
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
     addr.sin_port   = htons(static_cast<uint16_t>(config.port));
     if (::inet_pton(AF_INET, config.host.c_str(), &addr.sin_addr) <= 0) {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("inet_pton failed host='{}'", config.host), __FILE__, __LINE__});
+        log::trace({__func__, "ends (ListenAddressInvalid)", __FILE__, __LINE__});
+#endif
         ::close(fd);
         return ErrorStatus::ListenAddressInvalid;
     }
+#ifdef ENABLE_LOGGING
+    log::debug({__func__, std::format("inet_pton succeeded host='{}' port={}", config.host, config.port), __FILE__, __LINE__});
+#endif
     if (::bind(fd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0) {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("bind failed fd={} errno={} err='{}'", fd, errno, ::strerror(errno)), __FILE__, __LINE__});
+        log::trace({__func__, "ends (ListenSocketBindFailed)", __FILE__, __LINE__});
+#endif
         ::close(fd);
         return ErrorStatus::ListenSocketBindFailed;
     }
+#ifdef ENABLE_LOGGING
+    log::debug({__func__, std::format("bind succeeded fd={}", fd), __FILE__, __LINE__});
+#endif
     if (::listen(fd, SOMAXCONN) < 0) {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("listen failed fd={} errno={} err='{}'", fd, errno, ::strerror(errno)), __FILE__, __LINE__});
+        log::trace({__func__, "ends (ListenSocketListenFailed)", __FILE__, __LINE__});
+#endif
         ::close(fd);
         return ErrorStatus::ListenSocketListenFailed;
     }
+#ifdef ENABLE_LOGGING
+    log::debug({__func__, std::format("listen succeeded fd={}", fd), __FILE__, __LINE__});
+#endif
     out_fd = fd;
+#ifdef ENABLE_LOGGING
+    log::trace({__func__, "ends", __FILE__, __LINE__});
+#endif
     return ErrorStatus::OK;
 }
 
 ErrorStatus make_ssl_ctx(const tcp::Config& config, SSL_CTX*& out_ctx) noexcept {
+#ifdef ENABLE_LOGGING
+    log::trace({__func__, "begins", __FILE__, __LINE__});
+#endif
     SSL_CTX* ctx = SSL_CTX_new(TLS_server_method());
     if (!ctx) {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, "SSL_CTX_new failed", __FILE__, __LINE__});
+        log::trace({__func__, "ends (TlsContextCreationFailed)", __FILE__, __LINE__});
+#endif
         return ErrorStatus::TlsContextCreationFailed;
     }
+#ifdef ENABLE_LOGGING
+    log::debug({__func__, std::format("SSL_CTX_new succeeded ctx={}", (void*)ctx), __FILE__, __LINE__});
+#endif
     SSL_CTX_set_min_proto_version(ctx, TLS1_2_VERSION);
     SSL_CTX_set_max_proto_version(ctx, TLS1_3_VERSION);
+#ifdef ENABLE_LOGGING
+    log::debug({__func__, "proto version set TLS1.2-TLS1.3", __FILE__, __LINE__});
+#endif
     if (SSL_CTX_use_certificate_file(ctx, config.cert.c_str(), SSL_FILETYPE_PEM) <= 0) {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("SSL_CTX_use_certificate_file failed cert='{}'", config.cert), __FILE__, __LINE__});
+        log::trace({__func__, "ends (TlsCertificateLoadFailed)", __FILE__, __LINE__});
+#endif
         SSL_CTX_free(ctx);
         return ErrorStatus::TlsCertificateLoadFailed;
     }
+#ifdef ENABLE_LOGGING
+    log::debug({__func__, std::format("certificate loaded cert='{}'", config.cert), __FILE__, __LINE__});
+#endif
     if (SSL_CTX_use_PrivateKey_file(ctx, config.key.c_str(), SSL_FILETYPE_PEM) <= 0) {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("SSL_CTX_use_PrivateKey_file failed key='{}'", config.key), __FILE__, __LINE__});
+        log::trace({__func__, "ends (TlsPrivateKeyLoadFailed)", __FILE__, __LINE__});
+#endif
         SSL_CTX_free(ctx);
         return ErrorStatus::TlsPrivateKeyLoadFailed;
     }
+#ifdef ENABLE_LOGGING
+    log::debug({__func__, std::format("private key loaded key='{}'", config.key), __FILE__, __LINE__});
+#endif
     out_ctx = ctx;
+#ifdef ENABLE_LOGGING
+    log::trace({__func__, "ends", __FILE__, __LINE__});
+#endif
     return ErrorStatus::OK;
 }
 
 } // namespace
 
 slim::common::io::Task<void> Tcp::accept_loop(Tcp& self, slim::common::io::Runtime& runtime) {
-    log::trace(log::Message(__func__, "begins", __FILE__, __LINE__));
+#ifdef ENABLE_LOGGING
+    log::trace({__func__, "begins", __FILE__, __LINE__});
+#endif
     auto& dispatcher = runtime.dispatcher_scheduler();
     auto system_token = self.system_stop_token_;
     auto local_token  = self.local_stop_source_.get_token();
     while (!system_token.stop_requested() && !local_token.stop_requested()) {
         slim::common::io::Accept accept_op{dispatcher, self.listen_fd_};
         int client_fd = co_await accept_op;
-        if (system_token.stop_requested() || local_token.stop_requested()) break;
-        log::debug(log::Message(__func__, "co_await returned client_fd => " + std::to_string(client_fd), __FILE__, __LINE__));
-        if (client_fd < 0) break;
-        log::debug(log::Message(__func__, "posting connection handler for client_fd => " + std::to_string(client_fd), __FILE__, __LINE__));
+        if (system_token.stop_requested() || local_token.stop_requested()) {
+#ifdef ENABLE_LOGGING
+            log::debug({__func__, "stop requested, exiting accept loop", __FILE__, __LINE__});
+#endif
+            break;
+        }
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("co_await returned client_fd={}", client_fd), __FILE__, __LINE__});
+#endif
+        if (client_fd < 0) {
+#ifdef ENABLE_LOGGING
+            log::debug({__func__, std::format("client_fd={} < 0, breaking", client_fd), __FILE__, __LINE__});
+#endif
+            break;
+        }
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("posting connection handler for client_fd={}", client_fd), __FILE__, __LINE__});
+#endif
         SSL_CTX* ctx = self.ssl_ctx_;
         auto handler_copy = self.connection_handler_;
         runtime.post([client_fd, ctx, handler_copy](slim::common::io::Scheduler& worker_scheduler, size_t) {
@@ -81,45 +167,91 @@ slim::common::io::Task<void> Tcp::accept_loop(Tcp& self, slim::common::io::Runti
             worker_scheduler.spawn(std::move(conn));
         });
     }
-    log::trace(log::Message(__func__, "ends", __FILE__, __LINE__));
+#ifdef ENABLE_LOGGING
+    log::trace({__func__, "ends", __FILE__, __LINE__});
+#endif
 }
 
 Tcp::Tcp(const tcp::Config& config, slim::common::io::Runtime& runtime, std::stop_token stop_token,
     ConnectionHandler connection_handler) : system_stop_token_(std::move(stop_token)),
     stop_callback_(system_stop_token_, [this] {
-        log::debug(log::Message(__func__, "stop_callback_ invoked, shutdown listen_fd_ => " + std::to_string(listen_fd_), __FILE__, __LINE__));
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("stop_callback_ invoked, shutdown listen_fd_={}", listen_fd_), __FILE__, __LINE__});
+#endif
         ::shutdown(listen_fd_, SHUT_RDWR);
     }) {
-    log::trace(log::Message(__func__, "begins", __FILE__, __LINE__));
+#ifdef ENABLE_LOGGING
+    log::trace({__func__, "begins", __FILE__, __LINE__});
+#endif
     ErrorStatus status = make_listen_fd(config, listen_fd_);
     if (status != ErrorStatus::OK) {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("make_listen_fd failed status={}", static_cast<int>(status)), __FILE__, __LINE__});
+        log::trace({__func__, "ends (throw)", __FILE__, __LINE__});
+#endif
         throw NetworkException(status);
     }
-    log::debug(log::Message(__func__, "listen_fd_ => " + std::to_string(listen_fd_), __FILE__, __LINE__));
+#ifdef ENABLE_LOGGING
+    log::debug({__func__, std::format("listen_fd_={}", listen_fd_), __FILE__, __LINE__});
+#endif
     if (!config.cert.empty() && !config.key.empty()) {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("TLS configured cert='{}' key='{}'", config.cert, config.key), __FILE__, __LINE__});
+#endif
         status = make_ssl_ctx(config, ssl_ctx_);
         if (status != ErrorStatus::OK) {
+#ifdef ENABLE_LOGGING
+            log::debug({__func__, std::format("make_ssl_ctx failed status={}", static_cast<int>(status)), __FILE__, __LINE__});
+            log::trace({__func__, "ends (throw)", __FILE__, __LINE__});
+#endif
             ::close(listen_fd_);
             listen_fd_ = -1;
             throw NetworkException(status);
         }
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("ssl_ctx_={}", (void*)ssl_ctx_), __FILE__, __LINE__});
+#endif
+    } else {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, "no TLS configured, plain TCP", __FILE__, __LINE__});
+#endif
     }
     connection_handler_ = std::move(connection_handler);
     runtime.dispatcher_scheduler().post([this, &runtime]() {
-        log::debug(log::Message(__func__, "spawning accept_loop", __FILE__, __LINE__));
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, "spawning accept_loop", __FILE__, __LINE__});
+#endif
         runtime.dispatcher_scheduler().spawn(accept_loop(*this, runtime));
     });
-    log::trace(log::Message(__func__, "ends", __FILE__, __LINE__));
+#ifdef ENABLE_LOGGING
+    log::trace({__func__, "ends", __FILE__, __LINE__});
+#endif
 }
 
 Tcp::~Tcp() noexcept {
-    log::trace(log::Message(__func__, "begins", __FILE__, __LINE__));
+#ifdef ENABLE_LOGGING
+    log::trace({__func__, "begins", __FILE__, __LINE__});
+#endif
     local_stop_source_.request_stop();
-    log::debug(log::Message(__func__, "local_stop_source_ requested, shutdown listen_fd_ => " + std::to_string(listen_fd_), __FILE__, __LINE__));
+#ifdef ENABLE_LOGGING
+    log::debug({__func__, std::format("local_stop_source_ requested, shutdown listen_fd_={}", listen_fd_), __FILE__, __LINE__});
+#endif
     ::shutdown(listen_fd_, SHUT_RDWR);
-    if (ssl_ctx_)        SSL_CTX_free(ssl_ctx_);
-    if (listen_fd_ >= 0) ::close(listen_fd_);
-    log::trace(log::Message(__func__, "ends", __FILE__, __LINE__));
+    if (ssl_ctx_) {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("freeing ssl_ctx_={}", (void*)ssl_ctx_), __FILE__, __LINE__});
+#endif
+        SSL_CTX_free(ssl_ctx_);
+    }
+    if (listen_fd_ >= 0) {
+#ifdef ENABLE_LOGGING
+        log::debug({__func__, std::format("closing listen_fd_={}", listen_fd_), __FILE__, __LINE__});
+#endif
+        ::close(listen_fd_);
+    }
+#ifdef ENABLE_LOGGING
+    log::trace({__func__, "ends", __FILE__, __LINE__});
+#endif
 }
 
 } // namespace slim::common::network::server
